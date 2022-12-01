7319298e062d3a64e05c8009b77ab662432d296a
P-A Eriksson <per-arne.eriksson@skandia.se>
2022-12-01 09:12:08 +0100

    initial commit

---
+++ templateDay/mission.js
@@ -0,0 +1,16 @@
+import { getInputData } from '../lib/utils.js';
+
+const _inputPath = './dayDAYNUMBER/input.txt';
+
+function parser(inputData) {
+    return inputData.split(/\r?\n/);
+}
+
+function mission() {
+    let input = getInputData(_inputPath)(parser);
+
+    console.log(input);
+    console.log("End");
+}
+
+mission();
