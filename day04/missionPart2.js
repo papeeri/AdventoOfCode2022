9370189174467120b52ca0668011804c50819bdb
P-A Eriksson <p-a@peeri.se>
2022-12-04 19:02:52 +0100

    day 4 part 2

---
+++ day04/missionPart2.js
@@ -0,0 +1,32 @@
+// https://adventofcode.com/2022/day/4
+
+import { getInputData } from "../lib/utils.js";
+
+const _inputPath = "./day04/input.txt";
+
+function parser(inputData) {
+    let input = inputData.split(/\r?\n/).map((r) =>
+        r.split(",").map((e) => {
+            let s = e.split("-");
+            return { a: parseInt(s[0]), b: parseInt(s[1]) };
+        })
+    );
+
+    return input;
+}
+
+function mission() {
+    let input = getInputData(_inputPath)(parser);
+
+    let overlaps = 0;
+
+    input.map((p) => {
+        if ((p[0].a >= p[1].a && p[0].a <= p[1].b) || (p[1].a >= p[0].a && p[1].a <= p[0].b)) {
+            overlaps++;
+        }
+    });
+
+    console.log("Total overlaps:", overlaps);
+}
+
+mission();
