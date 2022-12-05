8a0c0017e5a2da3fe220d91665d6ee7b2b5a2964
P-A Eriksson <per-arne.eriksson@skandia.se>
2022-12-05 11:04:36 +0100

    day 5 part 1

--- day05/missionPart1.js
+++ day05/missionPart1.js
@@ -3,12 +3,25 @@
 import { getInputData } from "../lib/utils.js";
 import { parser } from "./parseInput.js";
 
-const _inputPath = "./day05/inputPart.txt";
+const _inputPath = "./day05/input.txt";
 
 function mission() {
     let { numberOfStacks, stacks, instructions } = getInputData(_inputPath)(parser);
 
-    console.log("End");
+    instructions.map((instruction) => {
+        for (let i = 0; i < instruction.move; i++) {
+            let item = stacks[instruction.from][0];
+            stacks[instruction.from].splice(0, 1);
+            stacks[instruction.to].unshift(item);
+        }
+    });
+
+    let answer = [];
+    for (let s = 1; s <= numberOfStacks; s++) {
+        answer.push(stacks[s][0]);
+    }
+
+    console.log("Message:", answer.join(""));
 }
 
 mission();
